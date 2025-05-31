19616ff06c053eafca0d3070009f0e5fd600bb4e
Shay Goldin <goldinshay@gmail.com>
2025-05-31 11:02:23 +0200

    Moved starter files into proper src/main/java structure

--- smart-home-system-starter/NotificationService.java
+++
@@ -1,8 +0,0 @@
-package com.smarthome;
-
-public class NotificationService {
-    public void notify(String message) {
-        // TODO: Integrate with your preferred notification mechanism 
-        System.out.println("[NOTIFICATION] " + message);
-    }
-}
